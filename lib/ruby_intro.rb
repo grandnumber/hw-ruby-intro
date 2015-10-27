48127544a43b875b7daac1f52b41cb61fb88f948
monkeypatching <thegrandnumber@gmail.com>
2015-10-27 02:50:16 +0000

    finish week 1 ruby_intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # return arr.empty? ? 0 : arr.inject(:+)
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  # arr.empty? ? 0 : arr.sort.last(2).inject(:+)
+  # refactor: uses the default value again
+  arr.sort.last(2).inject(0, :+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ # use binary approach to reduce the iterations 
+  left = 0
+  right = arr.length - 1
+  arr.sort!
+  while left < right
+    case arr[left] + arr[right] <=> n
+    when 0
+      return true
+    when 1
+      right -= 1
+    when -1
+      left += 1
+    end
+  end
+  false
 end
+
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s[0] =~ /[^aeiou\W]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /^([01]*0)?0$/
 end
 
 # Part 3
 
+# NB Should isbn be attr_reader given it should not change? 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError.new "ISBN must be non-empty string" unless
+      isbn.is_a?(String) && !isbn.empty?
+    raise ArgumentError.new "Price must be greather than 0" unless price > 0
+    @isbn, @price  = isbn, price
+  end
+
+  def price_as_string
+    %Q{$#{"%.2f" % @price}}
+  end
 end
+
